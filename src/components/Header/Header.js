1aa035f70bb08147ec81cb7506f5e9cae4426b36
k0502s <k0502s@naver.com>
2021-05-16 02:10:10 +0900

    Header 부분 Section 적용 및 네비 스크롤 링크 적용

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,14 @@ import HeaderImg from '../../assets/img/film.jpg';
 import * as S from './Header.style';
 const Header = () => {
     return (
-        <S.Hedaer style={{ backgroundImage: `url(${HeaderImg})` }}>
-            <h2>안녕하세요. 김진석입니다.</h2>
-            <p>프론트엔드 개발자가 되기를 희망하는 신입입니다.</p>
-        </S.Hedaer>
+        <>
+            <section id='home'>
+                <S.Hedaer style={{ backgroundImage: `url(${HeaderImg})` }}>
+                    <h2>안녕하세요. 김진석입니다.</h2>
+                    <p>프론트엔드 개발자가 되기를 희망하는 신입입니다.</p>
+                </S.Hedaer>
+            </section>
+        </>
     );
 };
 
