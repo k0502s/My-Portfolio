169be080aefda9aaf238d5007571e6ea51b9aba4
k0502s <k0502s@naver.com>
2021-05-16 01:39:29 +0900

    Header 컴포넌트 수정 및 CSS 스타일 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import HeaderImg from '../../assets/img/film.jpg';
+import * as S from './Header.style';
 const Header = () => {
     return (
-        <div>
-            헤더
-        </div>
-    )
-}
+        <S.Hedaer style={{ backgroundImage: `url(${HeaderImg})` }}>
+            <h2>안녕하세요. 김진석입니다.</h2>
+            <p>프론트엔드 개발자가 되기를 희망하는 신입입니다.</p>
+        </S.Hedaer>
+    );
+};
 
-export default Header
+export default Header;
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Hedaer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-size: cover;
+    width: 100%;
+    & h2 {
+        color: #fff;
+    }
+    & p {
+        color: #fff;
+    }
+`;
+
+export { Hedaer };
