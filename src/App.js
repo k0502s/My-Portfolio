fec88148563c276dbeb650f51cd8a16cf6b27739
k0502s <k0502s@naver.com>
2021-05-16 01:38:55 +0900

    styled-comonents 세팅 및 Globalstyle 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Main from './components/Main/Main'
-import Nav from './components/Nav/Nav'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+import AppNav from './components/Nav/Nav';
+import 'bootstrap/dist/css/bootstrap.css';
+import GlobalStyle from './assets/GlobalStyle';
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <Nav />
-      <Main/>
-      <Footer/>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <AppNav />
+            <Header />
+            <div id="main-body">
+                <Main />
+            </div>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/assets/GlobalStyle.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+    width: 100vw;
+    overflow-x: hidden;
+}
+body {
+    width: 100%;
+    height: 100%;
+    background-color: #47a0aa;
+    background-size: cover;
+    margin: 0;
+    padding: 0;
+    font-size: large;
+    font-family: 'Do Hyeon', sans-serif;
+    overflow-x: hidden;
+}
+*, *:before, *:after {
+    outline: none;
+    user-select: none;
+  }
+  a,
+  a:hover,
+  a:focus {
+      color: inherit;
+      text-decoration: none;
+      transition: all 0.3s;
+  }
+  #main-body {
+    min-height: 130vh;
+    margin-top: 100px;
+    @media only screen and (max-width: 767px) {
+    margin-top: 100px;
+}
+}
+  `;
+
+export default GlobalStyle;
