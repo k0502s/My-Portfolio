f1d2a546d3a9fbcd63cefa6fcf158002662ce78f
k0502s <k0502s@naver.com>
2021-06-24 14:05:04 +0900

    Contact 섹션 이메일 내용 수정 - 2

--- src/components/Main/Contact/Contact.js
+++ src/components/Main/Contact/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
             </S.TitleWrap>
             <S.InfoWrap>
                 <div>
-                    <S.EmailIcon /> <b>Email</b> kjs960502s@gmail.com
+                    <S.EmailIcon /> <b>Email</b> kjs960502@gmail.com
                 </div>
                 <div>
                     <S.PhoneIcon /> <b>Phone</b> 010-7921-7741
