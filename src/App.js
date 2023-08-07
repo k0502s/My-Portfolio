93745a8665728b6eeb53721dd72e158edfe24886
k0502s <k0502s@naver.com>
2023-08-07 22:24:55 +0900

    fix(test): git push test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import GlobalStyle from "./assets/GlobalStyle";
 
 import "bootstrap/dist/css/bootstrap.css";
