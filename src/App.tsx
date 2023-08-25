db69e339e3d442acd141c78360a7efd4a9693910
k0502s <k0502s@naver.com>
2023-08-25 21:41:29 +0900

    add(lightbox): react-18-image-lightbox 라이브러리 패키지 인스톨 및 환경세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import GlobalStyle from "@assets/GlobalStyle";
 import Main from "@pages/Main/Main";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "react-18-image-lightbox/style.css";
 
 const App = () => {
   return (
