654b441fac44afc7049a404a87e7927fd602dcfd
k0502s <k0502s@naver.com>
2023-08-30 02:37:24 +0900

    refactor(package): 안쓰는 라이브러리 패키지 삭제

--- src/pages/Main/Footer/Footer.style.tsx
+++ src/pages/Main/Footer/Footer.style.tsx
@@ -1,17 +1,16 @@
-import styled from 'styled-components';
-import { Row } from 'reactstrap';
+import styled from "styled-components";
 
-const FooterWrap = styled(Row)`
-    padding: 50px 0;
-    background-color: #091e2a;
-    color: #fff;
-    font-size: small;
-    font-family: none;
-    & p {
-        text-align: center;
-        letter-spacing: 0.2em;
-        margin: 0;
-    }
+const FooterWrap = styled.div`
+  padding: 50px 0;
+  background-color: #091e2a;
+  color: #fff;
+  font-size: small;
+  font-family: none;
+  & p {
+    text-align: center;
+    letter-spacing: 0.2em;
+    margin: 0;
+  }
 `;
 
 export { FooterWrap };
