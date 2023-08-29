50b25e0e617d18581a967b0eee135e8a8aa0534d
k0502s <k0502s@naver.com>
2023-08-29 22:26:45 +0900

    feat(useMediaQuery): useMediaQuery hooks 추가

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export const useMediaQuery = (width: number) => {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  const resizeWindow = useCallback(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  const result = useMemo(
+    () => (windowWidth ? windowWidth <= width : false),
+    [windowWidth, width],
+  );
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", resizeWindow);
+    return () => {
+      window.removeEventListener("resize", resizeWindow);
+    };
+  }, [windowWidth]);
+
+  return result;
+};
